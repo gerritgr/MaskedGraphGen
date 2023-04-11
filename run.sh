e48dba097e9ebd458d63ecfe6d23ea989f8e0a1a
Gerrit <gerritgr@users.noreply.github.com>
2023-04-11 17:06:14 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 cd /home/s9gtgros/MaskedGraphGen/
 jupyter nbconvert --to script --output train.py notebook.ipynb
 mv train.py.txt train.py
-conda env export > environment_export.yml
+conda env export > environment_export.yml.py # because py files will be uploaded to WandB automatically
 python train.py
