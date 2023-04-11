d796bbc4807845b7577fada4c7864d720f75c66e
Gerrit <gerritgr@users.noreply.github.com>
2023-04-11 17:05:15 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 cd /home/s9gtgros/MaskedGraphGen/
 jupyter nbconvert --to script --output train.py notebook.ipynb
 mv train.py.txt train.py
+conda env export > environment_export.yml
 python train.py
