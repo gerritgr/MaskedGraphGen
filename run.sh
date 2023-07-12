d5e4f15c93e5199c8444fe79527ec3c2edc27265
Gerrit <gerritgr@users.noreply.github.com>
2023-07-12 17:37:30 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,3 +5,4 @@ jupyter nbconvert --to script --output train.py notebook.ipynb
 mv train.py.txt train.py
 conda env export > environment_export.yml.py # because py files will be uploaded to WandB automatically
 python train.py
+
